04f259b04522fedddae39438f78380ddcf63e2cc
Dennis Alund <dennis.alund@gmail.com>
2021-09-14 14:37:46 +0800

    Changing channel name

--- lib/ethers.dart
+++ lib/ethers.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Ethers {
-  static const _channel = MethodChannel('famecapital_ethers');
+  static const _channel = MethodChannel('ethers');
 
   static Future<String?> get platformVersion =>
       _channel.invokeMethod('getPlatformVersion');
